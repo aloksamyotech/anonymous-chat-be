a557e37123fa4ee2c0895291b7664e66b2fabe5d
Deepak <deepak@samyotech.com>
2025-04-28 11:01:04 +0530

    write one to one chat app logic

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -21,13 +21,14 @@ export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
 
-
+  private onlineUsers = new Map<string, string>();
 
   constructor(private readonly messagesService: MessageService, private jwtService: JwtService) { }
 
   afterInit(server: Server) {
     server.use((socket: Socket, next) => {
-      const token = socket.handshake.headers?.token;
+      const token = socket.handshake.auth?.token || socket.handshake.query?.token;
+      console.log(`token`, token);
 
       if (!token || typeof token !== 'string') {
         return next(new Error('Unauthorized: No token provided'));
@@ -48,34 +49,77 @@ export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
     });
   }
 
-  handleConnection(client: Socket, ...args: any[]) {
+  async handleConnection(client: Socket, ...args: any[]) {
     console.log(`Client connected: ${client.id}`);
+    console.log("cl-----------", client.data.user);
+    const user = client.data.user;
+    if (user && user.id) {
+      this.onlineUsers.set(user.id, client.id);
+      console.log(`User ${user.id} connected with socket ${client.id}`);
 
+      const pendingMessages = await this.messagesService.getOfflineMessagesForUser(user.id);
+
+      if (pendingMessages.length > 0) {
+
+        pendingMessages.forEach((message) => {
+          client.emit('privateMessage', {
+            from: message.sender_id,
+            message: message.message,
+          });
+        });
+      }
+
+      this.messagesService.markMessagesAsDelivered(user.id)
+
+    }
   }
 
   handleDisconnect(client: Socket) {
     console.log(`Client disconnected: ${client.id}`);
+    const userId = [...this.onlineUsers.entries()].find(([, socketId]) => socketId === client.id)?.[0];
+    if (userId) {
+      this.onlineUsers.delete(userId); // Clean up the user from the map
+      console.log(`User ${userId} disconnected`);
+    }
   }
 
 
   @SubscribeMessage('message')
-  handleMessage(@MessageBody() message: any, @ConnectedSocket() client: Socket) {
+  async handleMessage(@MessageBody() message: any, @ConnectedSocket() client: Socket) {
     console.log(`Received message: ${message}`);
     console.log("cl-----------", client.data.user);
     console.log("message->", message);
     console.log("message->", message.type);
-    console.log("message->",message.payload.message);
-    
-    
+    console.log("message->", message.payload.message);
+
+
     // const parsedMessage = JSON.parse(message);
     const parsedMessage = message;
+    const sender = client.data.user;
 
     if (parsedMessage.type == "group-chat") {
-      this.messagesService.saveMessage(parsedMessage, client);
+      await this.messagesService.saveMessage(parsedMessage, client);
       this.server.emit('groupMessage', `Server received: ${parsedMessage.payload.message}`);
     }
-    
-    
+
+    if (parsedMessage.type === 'private-chat') {
+      const recipientId = parsedMessage.payload.recipientId;
+      const recipientSocketId = this.onlineUsers.get(recipientId);
+
+
+      if (recipientSocketId) {
+        await this.messagesService.saveMessage(parsedMessage, client);
+        this.server.to(recipientSocketId).emit('privateMessage', {
+          from: sender.id,
+          message: parsedMessage.payload.message,
+        })
+      } else {
+        await this.messagesService.saveOfflineMessage(parsedMessage, sender.id, recipientId);
+        console.log(`User ${recipientId} is not online`);
+      }
+    }
+
+
     // client.emit('messageResponse', `Acknowledged: ${parsedMessage.payload.message}`);
   }
 }
--- src/modules/message/message.service.ts
+++ src/modules/message/message.service.ts
@@ -8,18 +8,69 @@ export class MessageService {
         private readonly prisma: PrismaService,
 
     ) { }
-    async saveMessage(parsedMessage:any, client: Socket) {
+    async saveMessage(parsedMessage: any, client: Socket) {
         console.log(`Saving message to DB: ${parsedMessage}`);
 
-        const messageData = await this.prisma.message.create({
+        if (parsedMessage.type == "group-chat") {
+            const messageData = await this.prisma.message.create({
+                data: {
+                    isGroup: true,
+                    message: parsedMessage.payload.message,
+                    sender_id: client.data?.user?.id,
+                    status: 'DELIVERED'
+                }
+            })
+            console.log(messageData);
+        } else if (parsedMessage.type === 'private-chat') {
+            const messageData = await this.prisma.message.create({
+                data: {
+                    isGroup: false,
+                    message: parsedMessage.payload.message,
+                    sender_id: client.data?.user?.id,
+                    receiver_id: parsedMessage.payload.recipientId,
+                    status: 'DELIVERED'
+                }
+            })
+            console.log(messageData);
+        } else {
+            console.log("Invalid message type");
+        }
+    }
+
+    async saveOfflineMessage(parsedMessage: any, senderId: number, recipientId: number) {
+        const message = await this.prisma.message.create({
             data: {
-                isGroup: true,
+                isGroup: false,
                 message: parsedMessage.payload.message,
-                sender_id: client.data?.user?.id,
+                status: 'PENDING',
+                sender_id: senderId,
+                receiver_id: recipientId
             }
         })
 
-        console.log(messageData);
-        // Here you'd actually save to a DB
+        console.log("message offline==>", message);
+    }
+
+    async getOfflineMessagesForUser(userId: number) {
+        const messages =  await this.prisma.message.findMany({
+            where: {
+                receiver_id: userId,
+                status: 'PENDING'
+            }
+        })
+
+        return messages;
+    }
+
+    async markMessagesAsDelivered(userId: number) {
+        await this.prisma.message.updateMany({
+            where: {
+                receiver_id: userId,
+                status: 'PENDING'
+            },
+            data: {
+                status: 'DELIVERED'
+            }
+        })
     }
 }
